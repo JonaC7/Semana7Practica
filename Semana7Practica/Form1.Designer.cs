1546053985393e563cb391088f815075d4aa8d95
Jonathan Levi Carrillo Portillo <SMSS019222@ugb.edu.sv>
2022-09-11 22:38:14 -0600

    Semana 7 practica 2

--- Semana7Practica/Form1.Designer.cs
+++ Semana7Practica/Form1.Designer.cs
@@ -29,34 +29,112 @@ namespace Semana7Practica
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.comboOpciones = new System.Windows.Forms.ComboBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnItem = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnMostrar
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnMostrar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnMostrar.Location = new System.Drawing.Point(12, 37);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(12, 11);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(100, 20);
+            this.txtMensaje.TabIndex = 2;
+            this.txtMensaje.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboOpciones
+            // 
+            this.comboOpciones.FormattingEnabled = true;
+            this.comboOpciones.Items.AddRange(new object[] {
+            "Opcion 1",
+            "Opcion 2",
+            "Opcion 3",
+            "Opcion 4",
+            "Opcion 5"});
+            this.comboOpciones.Location = new System.Drawing.Point(654, 12);
+            this.comboOpciones.Name = "comboOpciones";
+            this.comboOpciones.Size = new System.Drawing.Size(121, 21);
+            this.comboOpciones.TabIndex = 3;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(153, 11);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 4;
+            this.txtNum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(153, 40);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 5;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(153, 66);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(100, 23);
+            this.btnSuma.TabIndex = 6;
+            this.btnSuma.Text = "Suma";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnItem
+            // 
+            this.btnItem.Location = new System.Drawing.Point(654, 55);
+            this.btnItem.Name = "btnItem";
+            this.btnItem.Size = new System.Drawing.Size(75, 23);
+            this.btnItem.TabIndex = 7;
+            this.btnItem.Text = "mostrar";
+            this.btnItem.UseVisualStyleBackColor = true;
+            this.btnItem.Click += new System.EventHandler(this.btnItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnItem);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.comboOpciones);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.btnMostrar);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.ComboBox comboOpciones;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnItem;
     }
 }
 
--- Semana7Practica/Form1.cs
+++ Semana7Practica/Form1.cs
@@ -16,5 +16,52 @@ namespace Semana7Practica
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, este es mi primer formulario");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensaje = txtMensaje.Text;
+            MessageBox.Show(mensaje);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtNum1.Text);
+            double num2 = double.Parse(txtNum2.Text);
+            if (num1 == 0 && num2 == 0)
+            {
+                MessageBox.Show("Ingreae datos");
+            }
+            var suma = num1 + num2;
+            MessageBox.Show(suma.ToString());
+        }
+
+        private void btnItem_Click(object sender, EventArgs e)
+        {
+            
+            string itemSelected = comboOpciones.Text;
+            switch(itemSelected)
+            {
+                case "Suma":
+                    break
+                case "Resta":
+                    break
+            }
+            MessageBox.Show(itemSelected);
+        }
     }
 }
