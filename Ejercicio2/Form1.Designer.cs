44adf4cd8ee50a80981f9a341687f6c99ac56056
Jonathan Levi Carrillo Portillo <SMSS019222@ugb.edu.sv>
2022-09-12 22:58:33 -0600

    Ejercicio2

---
+++ Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTiempo = new System.Windows.Forms.TextBox();
+            this.txtTasadeinteres = new System.Windows.Forms.TextBox();
+            this.txtCapital = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboInteres = new System.Windows.Forms.ComboBox();
+            this.btnCalculo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTiempo
+            // 
+            this.txtTiempo.Location = new System.Drawing.Point(77, 288);
+            this.txtTiempo.Name = "txtTiempo";
+            this.txtTiempo.Size = new System.Drawing.Size(100, 20);
+            this.txtTiempo.TabIndex = 1;
+            this.txtTiempo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtTasadeinteres
+            // 
+            this.txtTasadeinteres.Location = new System.Drawing.Point(478, 70);
+            this.txtTasadeinteres.Name = "txtTasadeinteres";
+            this.txtTasadeinteres.Size = new System.Drawing.Size(100, 20);
+            this.txtTasadeinteres.TabIndex = 2;
+            // 
+            // txtCapital
+            // 
+            this.txtCapital.Location = new System.Drawing.Point(77, 70);
+            this.txtCapital.Name = "txtCapital";
+            this.txtCapital.Size = new System.Drawing.Size(100, 20);
+            this.txtCapital.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Capital";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tasa de interes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tiempo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(475, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Periodo de tiempo";
+            // 
+            // comboInteres
+            // 
+            this.comboInteres.FormattingEnabled = true;
+            this.comboInteres.Items.AddRange(new object[] {
+            "Anual",
+            "Mensual",
+            "Diario"});
+            this.comboInteres.Location = new System.Drawing.Point(478, 287);
+            this.comboInteres.Name = "comboInteres";
+            this.comboInteres.Size = new System.Drawing.Size(121, 21);
+            this.comboInteres.TabIndex = 8;
+            this.comboInteres.SelectedIndexChanged += new System.EventHandler(this.comboInteres_SelectedIndexChanged);
+            // 
+            // btnCalculo
+            // 
+            this.btnCalculo.Location = new System.Drawing.Point(286, 336);
+            this.btnCalculo.Name = "btnCalculo";
+            this.btnCalculo.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculo.TabIndex = 9;
+            this.btnCalculo.Text = "Calcular";
+            this.btnCalculo.UseVisualStyleBackColor = true;
+            this.btnCalculo.Click += new System.EventHandler(this.btnCalculo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculo);
+            this.Controls.Add(this.comboInteres);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCapital);
+            this.Controls.Add(this.txtTasadeinteres);
+            this.Controls.Add(this.txtTiempo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTiempo;
+        private System.Windows.Forms.TextBox txtTasadeinteres;
+        private System.Windows.Forms.TextBox txtCapital;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboInteres;
+        private System.Windows.Forms.Button btnCalculo;
+    }
+}
+
---
+++ Ejercicio2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboInteres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+      
+        }
+
+        private void btnCalculo_Click(object sender, EventArgs e)
+        {
+            string itemSelected = comboInteres.Text;
+            double cap = double.Parse(txtCapital.Text);
+            double inte = double.Parse(txtTasadeinteres.Text);
+            double tiemp = double.Parse(txtTiempo.Text);
+            switch (itemSelected)
+            {
+                case "Anual":
+                    if (cap == 0 && inte == 0 && tiemp == 0)
+                    {
+                        MessageBox.Show("Ingrese datos");
+                    }
+                    var Anual = cap * inte * tiemp;
+                    MessageBox.Show(Anual.ToString());
+                    break;
+                case "Mensual":
+                    if (cap == 0 && inte == 0 && tiemp == 0)
+                    {
+                        MessageBox.Show("Ingrese datos");
+                    }
+                    var Mensual = cap * inte * (tiemp / 12);
+                    MessageBox.Show(Mensual.ToString());
+                    break;
+                case "Diario":
+                    if (cap == 0 && inte == 0 && tiemp == 0)
+                    {
+                        MessageBox.Show("Ingrese datos");
+                    }
+                    var Diario = cap * inte * (tiemp / 360);
+                    MessageBox.Show(Diario.ToString());
+                    break;
+            }
+                
+        }
+    }
+}
